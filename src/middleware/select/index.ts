a9bc879dd2f3272147102df8cdb5c14da0322a11
Michiel van der Velde <michiel@michielvdvelde.nl>
2019-07-24 16:52:59 +0200

    Add state type

--- src/middleware/select/index.ts
+++ src/middleware/select/index.ts
@@ -1,9 +1,10 @@
 'use strict'
 
 import Slim, { HttpContext } from '../../index'
+import { State } from '../../lib/Application'
 import { MiddlewareFunction } from '@art-of-coding/lime-compose'
 
-export function select (fn: (ctx: HttpContext) => boolean | Promise<boolean>, app: Slim): MiddlewareFunction<HttpContext> {
+export function select<S extends State = State> (fn: (ctx: HttpContext<S>) => boolean | Promise<boolean>, app: Slim): MiddlewareFunction<HttpContext<S>> {
   return async (ctx, next) => {
     let isSelected = fn(ctx)
 
