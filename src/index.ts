3b521cd3032dbd889a8b638eb591dc428a11c563
Michiel van der Velde <michiel@michielvdvelde.nl>
2019-08-14 23:15:08 +0200

    Export `Request', `Response`, `HttpError`

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,7 @@
 export { Application as default, State } from './lib/Application'
 export { HttpContext, createContext } from './lib/HttpContext'
+
+export { Request } from './lib/Request'
+export { Response } from './lib/Response'
+
+export { HttpError } from './util/HttpError'
